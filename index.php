81845f718e33154e5a2ae4a4d657f043120c25d7
Washiashi <s-yu.washiashi@rakuten.com>
2020-09-17 17:39:14 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>tech boost</title>
+
+//ここから追加
+  <style>
+  h1 {
+    color: red;
+  }
+  </style>
+//ここまで追加
+
+</head>
+  <body>
+    <h1>Hello world!!!</h1>
+    <p>CSSSSSSSSSの入門！</p>
+  </body>
+</html>
